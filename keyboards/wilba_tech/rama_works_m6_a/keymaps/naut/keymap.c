ba7bc842ac35be8908a273ba9b2071a1601505c4
naut <jrbarnachea@gmail.com>
2024-04-02 11:00:09 -0700

    Update m6_a

--- keyboards/wilba_tech/rama_works_m6_a/keymaps/naut/keymap.c
+++ keyboards/wilba_tech/rama_works_m6_a/keymaps/naut/keymap.c
@@ -2,15 +2,13 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    LAYOUT(
-        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6),
+    // Base Layer
+    [0] = LAYOUT(
+        TO(1),   KC_UP,   KC_PGDN,
+        KC_LEFT, KC_DOWN, KC_RGHT),
 
-    LAYOUT(
-        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-
-    LAYOUT(
-        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-
-    LAYOUT(
-        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO) };
-
+    // Fn1 Layer
+    [1] = LAYOUT(
+        TO(0),   KC_VOLU, KC_MUTE,
+        KC_MRWD, KC_VOLD, KC_MFFD),
+};
